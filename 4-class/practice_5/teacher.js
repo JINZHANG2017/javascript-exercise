e1cdadec9a8826918d1126a01ea4642dc7d17827
JINZHANG2017 <zj574406254@hotmail.com>
2020-09-21 20:35:00 +0800

    [ZhangJin] feat: finish exercise4 exercise4 prac 5

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,16 @@
 // TODO 20: 在这里写实现代码
+export default class Student {
+  constructor(name, age, klass) {
+    this.name = name;
+    this.age = age;
+    this.klass = klass;
+  }
+
+  introduce() {
+    if (this.klass) {
+      return `My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I teach Class ${this.klass}.`;
+    } else {
+      return `My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I teach No Class.`;
+    }
+  }
+}
