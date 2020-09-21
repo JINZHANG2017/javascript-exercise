7f22f4aeb32d0696a688c0d9e0277c242957edee
JINZHANG2017 <zj574406254@hotmail.com>
2020-09-21 19:54:31 +0800

    [ZhangJin] feat: finish exercise2 array_map

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,6 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  let str1 = 'a';
+  let ascii_of_a = str1.charCodeAt();
+  return collection.map(x => String.fromCharCode(ascii_of_a - 1 + x));
 }
