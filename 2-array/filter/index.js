157270c30a45829b2cdf8321bc578f93a4a60e6f
JINZHANG2017 <zj574406254@hotmail.com>
2020-09-21 19:44:36 +0800

    [ZhangJin] feat: finish exercise2 array_filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,14 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(x => x % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  //   return collection.filter(t1 => collection.filter(t2 => t1===t2).length <= 1);
+  return collection.filter(function(element, index, self) {
+    return self.indexOf(element) === index;
+  });
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
